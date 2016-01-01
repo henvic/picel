2791bc6ccb17dd023581be01fea2b65b940ff97a
Henrique Vicente <henriquevicente@gmail.com>
2015-12-31 22:27:00 -0300

    Using libwep 0.5.0.

--- .travis.install_deps.bash
+++ .travis.install_deps.bash
@@ -8,15 +8,15 @@ fi
 
 if [ "$TRAVIS_OS_NAME" == "linux" ]; then
   sudo apt-get install libjpeg-dev libpng-dev libtiff-dev libgif-dev
-  wget http://downloads.webmproject.org/releases/webp/libwebp-0.4.3.tar.gz
-  tar xzf libwebp-0.4.3.tar.gz
-  rm libwebp-0.4.3.tar.gz
-  cd libwebp-0.4.3
+  wget http://downloads.webmproject.org/releases/webp/libwebp-0.5.0.tar.gz
+  tar xzf libwebp-0.5.0.tar.gz
+  rm libwebp-0.5.0.tar.gz
+  cd libwebp-0.5.0
   ./configure --enable-everything
   make
   sudo make install
   cd ..
-  rm -rf libwebp-0.4.3
+  rm -rf libwebp-0.5.0
 
   sudo apt-get update
   sudo apt-get install build-essential libx11-dev libxext-dev zlib1g-dev libpng12-dev libjpeg-dev libfreetype6-dev libxml2-dev
