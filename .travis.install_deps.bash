e22b84e309fc123ccd880b6bac0a45e18a6dae5c
Henrique Vicente <henriquevicente@gmail.com>
2016-11-22 15:56:31 -0300

    Using libwebp 0.5.1 on Travis.

--- .travis.install_deps.bash
+++ .travis.install_deps.bash
@@ -8,15 +8,15 @@ fi
 
 if [ "$TRAVIS_OS_NAME" == "linux" ]; then
   sudo apt-get install libjpeg-dev libpng-dev libtiff-dev libgif-dev
-  wget http://downloads.webmproject.org/releases/webp/libwebp-0.5.0.tar.gz
-  tar xzf libwebp-0.5.0.tar.gz
-  rm libwebp-0.5.0.tar.gz
-  cd libwebp-0.5.0
+  wget http://downloads.webmproject.org/releases/webp/libwebp-0.5.1.tar.gz
+  tar xzf libwebp-0.5.1.tar.gz
+  rm libwebp-0.5.1.tar.gz
+  cd libwebp-0.5.1
   ./configure --enable-everything
   make
   sudo make install
   cd ..
-  rm -rf libwebp-0.5.0
+  rm -rf libwebp-0.5.1
 
   sudo apt-get update
   sudo apt-get install build-essential libx11-dev libxext-dev zlib1g-dev libpng12-dev libjpeg-dev libfreetype6-dev libxml2-dev
